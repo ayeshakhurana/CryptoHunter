652afae66fe905c7804fae98974abc27270a072c
ayeshakhurana <ayesha@Ayeshas-MacBook-Air.local>
2025-04-12 16:10:01 +0530

    made banner

---
+++ crypto-hunter/src/Components/Banner/Banner.js
@@ -0,0 +1,60 @@
+import { Container, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import React from 'react';
+
+const useStyles = makeStyles(() => ({
+  banner: {
+    backgroundImage: "url(/image.png)", // Correct path from public folder
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  bannerContent: {
+    height: 400,
+    display: "flex",
+    flexDirection: "column",
+    paddingTop: 25,
+    justifyContent: "space-around",
+  },
+  tagline: {
+    display: "flex",
+    height: "40%",
+    flexDirection: "column",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+}));
+
+const Banner = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.banner}>
+      <Container className={classes.bannerContent}>
+        <div className={classes.tagline}>
+          <Typography
+            variant="h2"
+            style={{
+              fontWeight: "bold",
+              marginBottom: 15,
+              fontFamily: "Montserrat",
+            }}
+          >
+            Crypto Hunter
+          </Typography>
+          <Typography
+            variant="subtitle2"
+            style={{
+              color: "darkgrey",
+              textTransform: "capitalize",
+              fontFamily: "Montserrat",
+            }}
+          >
+            Get all the Info regarding your favorite Crypto Currency
+          </Typography>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Banner;
